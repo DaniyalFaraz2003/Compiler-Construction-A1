a6b4bf97bb138ae0b32f95b70666e33d107fa492
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2025-02-19 22:52:54 +0500

    scanner code being written

--- src/compiler/Scanner.java
+++ src/compiler/Scanner.java
@@ -1,5 +1,26 @@
 package compiler;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Scanner {
+    ArrayList<String> tokenList;
 
+    public Scanner() {
+        this.tokenList = new ArrayList<>();
+    }
+
+    private boolean isSingleToken(String token) {
+        return Objects.equals(token, "(") || Objects.equals(token, ")") ||
+                Objects.equals(token, "{") || Objects.equals(token, "}") || Objects.equals(token, "[") ||
+                Objects.equals(token, "]") || Objects.equals(token, ";") || Objects.equals(token, ",") ||
+                Objects.equals(token, "+") || Objects.equals(token, "-") || Objects.equals(token, "*") ||
+                Objects.equals(token, "/") || Objects.equals(token, "%") || Objects.equals(token, "^") ||
+                Objects.equals(token, "=");
+    }
+
+    private boolean isOperator(String token) {
+        return Objects.equals(token, "+") || Objects.equals(token, "-") || Objects.equals(token, "*") ||
+                Objects.equals(token, "/") || Objects.equals(token, "%") || Objects.equals(token, "^")
+                || Objects.equals(token, "=");
+    }
 }
