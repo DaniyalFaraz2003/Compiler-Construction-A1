7c81a3556366a535abc9008dc06552117d6945a6
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2025-02-20 19:06:03 +0500

    Assignment Completed

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Sample code to test - you can also load from file using lexer.loadCodeFromFile()
 
 
         // Initialize components
